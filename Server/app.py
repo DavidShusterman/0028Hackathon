06f0f0f0d53970b5435aafe54c7867b1a2b60f84
DavidShusterman <davidshusti@gmail.com>
2017-08-22 00:04:13 +0300

    chore: finished server boilerplate

--- Server/app.py
+++ Server/app.py
@@ -2,18 +2,25 @@ import os
 import tornado.ioloop
 import tornado.web
 import motor.motor_tornado
+from Handlers.HelloWorldHandler import HelloWorldHandler
 
 root = os.path.dirname(__file__)
 
 
-def create_server_application(mongo_db_client,index_file):
+def create_server_application(mongo_db_client):
     return tornado.web.Application([
-        (r"/helloworld", ))
-
+        (r"/helloworld",HelloWorldHandler)])
 
 
 def main():
-    pass
+    mongo_connection_string = 'localhost'
+    mongo_db_name = 'hackathonDB'
+    mongo_db_connection = motor.motor_tornado.MotorClient(mongo_connection_string)
+    mongo_db_client = mongo_db_connection[mongo_db_name]
+
+    app = create_server_application(mongo_db_client)
+    app.listen(port=8000,address="0.0.0.0")
+    tornado.ioloop.IOLoop.current().start()
 
 
 
