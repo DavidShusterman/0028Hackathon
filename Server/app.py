b0b499955d702114ee4e79cf59734182f732459a
DavidShusterman <davidshusti@gmail.com>
2017-08-22 00:01:01 +0300

    fix: updated server

--- Server/app.py
+++ Server/app.py
@@ -11,24 +11,9 @@ def create_server_application(mongo_db_client,index_file):
         (r"/helloworld", ))
 
 
-def write_authenticated_user_to_server(username,password,dbhandler):
-    dbhandler.Authenticated.insert_one({"username":username,"password":password})
-
 
 def main():
-    cfg_path = get_configuration_file()
-    config_reader = ConfigReader(cfg_path)
-    mongo_connection_string = os.environ.get('MONGO_CONN_STRING','localhost')
-    mongo_db_name = config_reader.get_value("mongoDBName")
-    mongo_db_connection = motor.motor_tornado.MotorClient(mongo_connection_string)
-    mongo_db_client = mongo_db_connection[mongo_db_name]
-    publisher = get_publisher(cfg_path)
-    index_file = get_index_file()
-    app = create_server_application(mongo_db_client,publisher,index_file)
-    server_port = config_reader.get_value("serverPort")
-    server_host= config_reader.get_value("serverHost")
-    app.listen(server_port,address=server_host)
-    tornado.ioloop.IOLoop.current().start()
+    pass
 
 
 
